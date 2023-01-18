30f798fcb0ef410f8e326ee42d332569ffc7ed93
Matheus Alves <mathfrontend@gmail.com>
2023-01-17 23:10:37 -0300

    fixing wrong writes in model schema and updating the relations with standard names

--- src/prisma/signaturesRelation.ts
+++ src/prisma/signaturesRelation.ts
@@ -8,7 +8,7 @@ export async function createSubscriberRelation(SubscriberId: string, ClubProvide
         id: SubscriberId
       },
       data: {
-        signatures: {
+        signaturesClubs: {
           connect: [{id: ClubProviderId}]
         }
       }
@@ -23,7 +23,7 @@ export async function removeSubscriberRelation(SubscriberId: string, ClubProvide
       id: SubscriberId
     },
     data: {
-      signatures: {
+      signaturesClubs: {
         disconnect: [{id: ClubProviderId}]
       }
     }
